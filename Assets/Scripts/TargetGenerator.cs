b586951e3c2a07661f5d87808b5d1a896ca885b6
Reshybraz <59066353+Reshybraz@users.noreply.github.com>
2024-02-11 15:08:36 +0100

    Merge branch 'target'

---
+++ Assets/Scripts/TargetGenerator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetGenerator : MonoBehaviour
+{
+    public Vector2 upperBoundary,lowerBoundary;
+    public GameObject targetPrefab;
+    private bool targetOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!targetOn) 
+        {
+            float posx = Random.Range(lowerBoundary.x,upperBoundary.x);
+            float posy = Random.Range(lowerBoundary.y,upperBoundary.y);
+            GameObject newTarget = Instantiate(targetPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            newTarget.transform.parent = transform;
+            newTarget.transform.localPosition = new Vector3(posx, 0, posy);
+            targetOn= true;
+        }
+    }
+
+    public void TargetGotShot() { targetOn = false; }
+}
